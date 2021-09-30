02ea0309ccbc745d66e40b5e4d211de60527e804
Yudota <yuri.fernandes.961@gmail.com>
2021-09-30 00:00:56 -0300

    exclude imports

--- src/pages/AddProvider.tsx
+++ src/pages/AddProvider.tsx
@@ -1,7 +1,4 @@
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
 import React from "react";
-import TableProviders from "../components/HomePageWrapper";
 import PageHeader from "../components/PageHeader";
 
 export default function Home() {
